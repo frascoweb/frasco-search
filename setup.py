a604c245fa485b1f44dae9fd4bf8ec4c6eebfac2
maximebf <maxime.bouroumeau@gmail.com>
2014-09-24 10:27:29 +0200

    added requirements.txt

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ def desc():
     with open("README.md") as f:
         return f.read()
 
+def reqs():
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
 
 setup(
     name='frasco-search',
@@ -17,8 +20,5 @@ setup(
     long_description=desc(),
     py_modules=['frasco_search'],
     platforms='any',
-    install_requires=[
-        'frasco',
-        'elasticsearch==1.2.0'
-    ]
+    install_requires=reqs() + ['frasco']
 )
